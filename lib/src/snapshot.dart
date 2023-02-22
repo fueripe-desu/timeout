a74ddfab2595e19bc9e161eb8cd78e25a23127ac
fueripe-desu <fueripedesu2020@outlook.com>
2023-02-22 14:45:59 -0300

    feat(snapshot): :sparkles: implement Snapshot class constructor
    The intial approach to the Snapshot class is an immutable class that holds a specific point on time, it will be considered as a primitive.

--- lib/src/snapshot.dart
+++ lib/src/snapshot.dart
@@ -1,1 +1,20 @@
-class Snapshot {}
+// A Snapshot is an immutable point on time
+class Snapshot {
+  int milisecond;
+  int second;
+  int minute;
+  int hour;
+  int day;
+  int month;
+  int year;
+
+  Snapshot({
+    this.milisecond = 0,
+    this.second = 0,
+    this.minute = 0,
+    this.hour = 0,
+    this.day = 1,
+    this.month = 1,
+    this.year = 1,
+  });
+}
