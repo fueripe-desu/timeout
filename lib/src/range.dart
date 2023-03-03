cbc1ed2f15f4a5b902d11d3840048736607b5351
fueripe-desu <fueripedesu2020@outlook.com>
2023-03-03 13:07:31 -0300

    feat(range): :sparkles: add Range constructor

--- lib/src/range.dart
+++ lib/src/range.dart
@@ -1,1 +1,13 @@
-class Range {}
+import 'package:meta/meta.dart';
+import 'package:timeout/src/snapshot.dart';
+
+@immutable
+class Range {
+  final Snapshot initialDate;
+  final Snapshot endDate;
+
+  const Range(
+    this.initialDate,
+    this.endDate,
+  );
+}
