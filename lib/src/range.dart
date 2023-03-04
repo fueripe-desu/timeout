ee343b6d28bb2c5b8872621bb1488db00916254d
fueripe-desu <fueripedesu2020@outlook.com>
2023-03-04 06:32:11 -0300

    feat(range): :sparkles: add getter for difference in Duration, add time constants
    Add a getter that allows to get the difference between two dates in a Duration object, so instead of returning all in one time unit, it returns it breaking down into different time units (eg. 2 days,  4 hour, 34 minutes, 27 seconds, 213 milliseconds of difference).

--- lib/src/range.dart
+++ lib/src/range.dart
@@ -1,4 +1,5 @@
 import 'package:meta/meta.dart';
+import 'package:timeout/src/constants.dart';
 import 'package:timeout/src/snapshot.dart';
 
 @immutable
@@ -18,4 +19,38 @@ class Range {
   int get differenceInMinutes => differenceInSeconds ~/ 60;
   int get differenceInHours => differenceInMinutes ~/ 60;
   int get differenceInDays => differenceInHours ~/ 24;
+
+  Duration get difference => _calculdateDifferece(initialDate, endDate);
+
+  Duration _calculdateDifferece(Snapshot intial, Snapshot end) {
+    final dEpoch = end.epochTime - intial.epochTime;
+    int remainder = 0;
+
+    // Calculate entire days with remainder
+    final days = dEpoch ~/ TimeConstants.millisecondsInDay;
+    remainder = dEpoch - (TimeConstants.millisecondsInDay * days);
+
+    // Calculate entire hours with remainder
+    final hours = remainder ~/ TimeConstants.millisecondsInHour;
+    remainder = remainder - (TimeConstants.millisecondsInHour * hours);
+
+    // Calculate entire minutes with remainder
+    final minutes = remainder ~/ TimeConstants.millisecondsInMinute;
+    remainder = remainder - (TimeConstants.millisecondsInMinute * minutes);
+
+    // Calculate entire seconds with remainder
+    final seconds = remainder ~/ TimeConstants.millisecondsInSecond;
+    remainder = remainder - (TimeConstants.millisecondsInSecond * seconds);
+
+    // Remaining milliseconds
+    final milliseconds = remainder;
+
+    return Duration(
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+      milliseconds: milliseconds,
+    );
+  }
 }
